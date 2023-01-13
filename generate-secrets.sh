7b35a12967da24ed5b40060aa4a3caa8378cec27
Alec Burgdorf <aeburgd@gmail.com>
2023-01-13 19:55:19 +0000

    chore: remove authelia details

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -1,18 +1,8 @@
 #! /bin/sh
-# vaultwarden secrets
-# postgresql secrets
-# authelia secrets
 
 COMMAND=$1 && shift 1
 
 case "$COMMAND" in
-	'authelia' )
-		openssl rand -hex 32 > secrets/authelia/auth_jwt_secret
-		openssl rand -hex 32 > secrets/authelia/auth_oidc_hmac_secret
-		openssl rand -hex 32 > secrets/authelia/auth_session_redis_password
-		openssl rand -hex 32 > secrets/authelia/auth_session_secret
-		openssl rand -hex 32 > secrets/authelia/auth_smtp_password
-		;;
 	'vaultwarden' )
 		openssl rand -hex 32 > secrets/vw_admin_token
 		;;
