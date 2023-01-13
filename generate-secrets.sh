0f3c639d0b737660161ebac78903c1dd101b1ea3
Alec Burgdorf <aeburgd@gmail.com>
2023-01-13 19:33:46 +0000

    refactor: move secret creation to functions for services

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -1,3 +1,27 @@
 #! /bin/sh
-openssl rand -base64 32 >> secrets/vw_admin_token
-openssl rand -base64 32 >> secrets/pg_user_password
+# vaultwarden secrets
+# postgresql secrets
+# authelia secrets
+
+COMMAND=$1 && shift 1
+
+case "$COMMAND" in
+	'authelia' )
+		openssl rand -hex 32 > secrets/authelia/auth_jwt_secret
+		openssl rand -hex 32 > secrets/authelia/auth_oidc_hmac_secret
+		openssl rand -hex 32 > secrets/authelia/auth_session_redis_password
+		openssl rand -hex 32 > secrets/authelia/auth_session_secret
+		openssl rand -hex 32 > secrets/authelia/auth_smtp_password
+		;;
+	'vaultwarden' )
+		openssl rand -hex 32 > secrets/vw_admin_token
+		;;
+	'postgres' )
+		openssl rand -hex 32 > secrets/pg_user_password
+		;;
+	'*' )
+		echo "Unknown service"
+		;;
+esac
+
+exec "$@"
