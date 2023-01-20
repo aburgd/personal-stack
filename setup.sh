6d340c9d220c294f9fe4f9416113aa02916ab199
Alec Burgdorf <aeburgd@gmail.com>
2023-01-20 14:18:24 +0000

    refactor: move generate-secrets.sh to setup.sh for env

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+COMMAND=$1 && shift 1
+
+case "$COMMAND" in
+	'env' )
+		echo "LOG_FILE=/data/access.log
+EMAIL=‘replace@email.com’
+VAULT_DOMAIN=‘https://vault.doma.in’
+DB_DOMAIN=‘https://db.doma.in’" > caddy.env
+		echo "POSTGRES_USER=postgres
+POSTGRES_PASSWORD_FILE=/run/secrets/db_user_password
+POSTGRES_DB=postgres" > db.env
+		touch nocodb.env
+		;;
+	'vaultwarden' )
+		openssl rand -hex 32 > secrets/vw_admin_token
+		;;
+	'postgres' )
+		openssl rand -hex 32 > secrets/pg_user_password
+		;;
+	'*' )
+		echo "Unknown service"
+		;;
+esac
+
+exec "$@"
