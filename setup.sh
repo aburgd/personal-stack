50b662530f4e5607a5f4f85388a49bcc75a131f4
Alec Burgdorf <aeburgd@gmail.com>
2023-02-09 21:48:12 +0000

    chore: add adguard to setup script

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ POSTGRES_DB=postgres" > db.env
 		;;
 	'vaultwarden' )
 		openssl rand -hex 32 > secrets/vw_admin_token
+		echo "[INFO] Vaultwarden admin token file created"
 		;;
 	'postgres' )
 		openssl rand -hex 32 > secrets/pg_user_password
@@ -26,6 +27,11 @@ POSTGRES_DB=postgres" > db.env
 		;;
 	'env' )
 		cp sample/* ./
+		;;
+	'adguard' )
+		echo "[INFO] Download and run lego (github.com/go-acme/lego) for your domain with
+[INFO] the --path argument to a well-known path, and put that path in a volume
+[INFO] in docker-compose.yml"
 		;;
 	'*' )
 		echo "Unknown service"
